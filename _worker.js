46f6e99f3531069a9c79e405315dcc00c836e5ec
Bharatro <87863921+Bharatro@users.noreply.github.com>
2026-05-17 11:41:44 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -447,7 +447,8 @@ async function getSUB(api, request, 追加UA, userAgentHeader) {
 async function getUrl(request, targetUrl, 追加UA, userAgentHeader) {
 	// 设置自定义 User-Agent
 	const newHeaders = new Headers(request.headers);
-	newHeaders.set("User-Agent", `${atob('djJyYXlOLzYuNDU=')} cmliu/CF-Workers-SUB ${追加UA}(${userAgentHeader})`);
+	const fakeUA = 追加UA.toLowerCase().includes('clash') ? 'clash-verge/v2.4.5' : `${atob('djJyYXlOLzYuNDU=')} cmliu/CF-Workers-SUB ${追加UA}(${userAgentHeader})`;
+	newHeaders.set("User-Agent", fakeUA);
 
 	// 构建新的请求对象
 	const modifiedRequest = new Request(targetUrl, {
